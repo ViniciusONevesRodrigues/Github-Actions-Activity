54f05f42a60196c245249c64fe75e4668bb1f7d6
ViniciusONevesRodrigues <vinicius.oliveira.neves.rodrigues@gmail.com>
2024-10-16 19:23:17 -0300

    Testando Vercel 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apredendo a usar GitHub Actions
+          Learn React
         </a>
       </header>
     </div>
